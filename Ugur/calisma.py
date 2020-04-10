8f2d96baf66bf384a2646a31db7db67187f8bc8a
ugurdikici <62259649+ugurdikici@users.noreply.github.com>
2020-04-10 21:20:19 +0300

    MenuEklendi_Ugur

--- Ugur/calisma.py
+++ Ugur/calisma.py
@@ -48,9 +48,51 @@ class TelefonDefter:
         finally:
             self.db.commit()
 
+    def Listeleme(self):
+        try:
+            sorgu =f"""SELECT * FROM telefonlar"""
+            self.cur.execute(sorgu)
+            liste = self.cur.fetchall()
+            for tel_id,adi,soyadi,tel_no in liste:
+                print(f"{tel_id}-{adi} {soyadi} {tel_no}")
+        except Exception as hata: 
+            print("Hata oluştu.",hata)
+            return hata
 
+    def Menu(self):
+        menu = """
+        1-Ekleme
+        2-Silme
+        3-Güncelleme
+        4-Listeleme
+        5-Çıkış
+        """
+        islem = 0
+        while islem < 6:
+            islem = int(input(menu))
+            if islem != 5:
+                if islem ==1:
+                    self.Ekleme()
+                elif islem == 2:
+                    self.Listeleme()
+                    secid = int(input("Silmek istediğiniz kaydın numarasını giriniz"))
+                    self.Silme(secid)
+                elif islem == 3:
+                    self.Listeleme()
+                    secid = int(input("Güncellemek istediğiniz kaydın numarasını giriniz"))
+                    self.Silme(secid)
+                elif islem == 4:
+                    self.Listeleme()
+            else:
+                islem = 6
+        else:
+            print("İyi Günler Dİleriz")
+                
 
     def __del__(self):
         self.cur.close()
         self.db.commit()
         self.db.close()
+        
+telDefter = TelefonDefter()
+telDefter.Menu()
