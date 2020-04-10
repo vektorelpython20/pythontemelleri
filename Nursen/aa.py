a5141b9d70238233a4da00cf5d8c4483f1c4cdb8
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-04-10 20:32:20 +0300

    düzetme Nursen

--- Nursen/aa.py
+++ Nursen/aa.py
@@ -1,9 +1,27 @@
-
-        except exception as hata:
-            print("hata oluştu.",hata)
+import sqlite3 as sql
+class TelefonDefter:
+    def __init__(self):
+        self.db = sql.connect("DB/IK.sqlite")
+        self.cur = self.db.cursor()
+    
+    def Ekleme(self):
+        try:
+            adi = input("Adınızı Giriniz: ")
+            soyadi = input("Soyadınızı Giriniz: ")
+            telefon = input("Telefon numaranızı giriniz: ")
+            sorgu = f"""
+            INSERT INTO telefonlar (adi,soyadi,tel_no)
+            VALUES
+            ('{adi}','{soyadi}','{telefon}')
+            """
+            self.cur.execute(sorgu)
+            return 1
+        except Exception as hata: 
+            print("Hata oluştu.",hata)
             return hata
         finally:
             self.db.commit()
+            
     def Guncelleme(self,id):
         try:
             adi = input("Adınızı Giriniz: ")
