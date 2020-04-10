a4b4b45c1fb0a12884b6b11d54aae429f948242b
hakanozgoren <63244095+hakanozgoren@users.noreply.github.com>
2020-04-10 19:55:41 +0300

    INSERTSORGUEKLENDI_Ediz

---
+++ Hakan/aaa.py
@@ -0,0 +1,19 @@
+import sqlite3 as sql
+try:
+    db = sql.connect("DB/IK.sqlite")
+    cur = db.cursor()
+    adi = input("Adınızı Giriniz: ")
+    soyadi = input("Soyadınızı Giriniz: ")
+    telefon = input("Telefon numaranızı giriniz: ")
+    sorgu = f"""
+    INSERT INTO telefonlar (adi,soyadi,tel_no)
+    VALUES
+    ('{adi}','{soyadi}','{telefon}')
+    """
+    cur.execute(sorgu)
+except Exception as hata: 
+    print("Hata oluştu.",hata)
+finally: 
+    cur.close()
+    db.commit()
+    db.close()
