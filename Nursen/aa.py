54282ca57bddb47fd099618c21e012e844e26b99
nursen02 <nurseneg@gmail.com>
2020-04-10 20:43:56 +0300

    SİLMESORGUEKLEME_NURSEN

--- Nursen/aa.py
+++ Nursen/aa.py
@@ -1,9 +1,33 @@
-
-        except exception as hata:
-            print("hata oluştu.",hata)
+  import sqlite3 as sql
+class TelefonDefter:
+    def __init__(self):
+        self.db = sql.connect("DB/IK.sqlite")
+        self.cur = self.db.cursor()
+    
+    def Ekleme(self):
+        try:
+            adi = input("Adınızı Giriniz: ")
+            soyadi = input("Soyadınızı Giriniz: ")
+            telefon = input("Telefon numaranızı giriniz: ")
+            sorgu = f"""
+            INSERT INTO telefonlar (adi,soyadi,tel_no)
+            VALUES
+            ('{adi}','{soyadi}','{telefon}')
+            """
+            self.cur.execute(sorgu)
+            return 1
+        except Exception as hata: 
+            print("Hata oluştu.",hata)
             return hata
         finally:
             self.db.commit()
+            
+    def __del__(self):
+        self.cur.close()
+        self.db.commit()
+        self.db.close()
+
+        
     def Guncelleme(self,id):
         try:
             adi = input("Adınızı Giriniz: ")
@@ -20,6 +44,18 @@
             return hata
         finally:
             self.db.commit()
+    
+    def Silme(self,id)
+        try:
+            sorgu = f""" DELETE * FROM telefonlar WHERE tel_id = {id} """
+            self.cur.execute(sorgu)
+            return 1
+        except Exception as hata:
+            print("hata oluştu.",hata)
+            return hata
+        finally:
+            self.db.commit()
+
             
     def __del__(self):
         self.cur.close()
