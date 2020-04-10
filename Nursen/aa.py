6cf08c3d736b4330a35df820b4e3eac1ffe01742
nursen02 <nurseneg@gmail.com>
2020-04-10 19:56:38 +0300

    INSERTSORGUEKLENDİ_Nursen

--- Nursen/aa.py
+++ Nursen/aa.py
@@ -1,4 +1,21 @@
 import sqlite3 as sql
 db = sql.connect("DB/IK.sqlite")
 cur = db.cursor()
-cur.execute("")
+adi = input("Adınızı Giriniz:")
+soyadi =input("Soyadınızı Giriniz:")
+telefon = input("Telefon numaranızı Giriniz:")
+sorgu = f""""
+INSERT INTO telefonlar (adi,soyadi,tel_no)
+VALUES
+('{adi}','{soyadi}','{telefon}')
+"""
+cur.execute(sorgu)
+except Exception as hata:
+    print("hata oluştu",hata)
+finally:
+    cur.close()
+    db.commit()
+    db.close()
+# insert #
+# delete #
+# update #
