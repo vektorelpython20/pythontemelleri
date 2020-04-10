5eedcf808ed73727fce80bf5968a807041ff442e
nursen02 <nurseneg@gmail.com>
2020-04-10 20:29:50 +0300

    updatesorgusueklendi_nursen

--- Nursen/aa.py
+++ Nursen/aa.py
@@ -1,21 +1,28 @@
-import sqlite3 as sql
-db = sql.connect("DB/IK.sqlite")
-cur = db.cursor()
-adi = input("Adınızı Giriniz:")
-soyadi =input("Soyadınızı Giriniz:")
-telefon = input("Telefon numaranızı Giriniz:")
-sorgu = f""""
-INSERT INTO telefonlar (adi,soyadi,tel_no)
-VALUES
-('{adi}','{soyadi}','{telefon}')
-"""
-cur.execute(sorgu)
-except Exception as hata:
-    print("hata oluştu",hata)
-finally:
-    cur.close()
-    db.commit()
-    db.close()
-# insert #
-# delete #
-# update #
+
+        except exception as hata:
+            print("hata oluştu.",hata)
+            return hata
+        finally:
+            self.db.commit()
+    def Guncelleme(self,id):
+        try:
+            adi = input("Adınızı Giriniz: ")
+            soyadi = input("Soyadınızı Giriniz: ")
+            telefon = input("Telefon numaranızı giriniz: ")
+            sorgu = f"""
+            UPDATE telefonlar SET adi = '{adi}',soyadi='{soyadi}',tel_no'{telefon}''
+            WHERE tel_id = {id}
+            """
+            self.cur.execute(sorgu)
+            return 1
+        except Exception as hata: 
+            print("Hata oluştu.",hata)
+            return hata
+        finally:
+            self.db.commit()
+            
+    def __del__(self):
+        self.cur.close()
+        self.db.commit()
+        self.db.close()
+
