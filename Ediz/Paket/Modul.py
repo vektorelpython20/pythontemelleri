dd4d2ed9eba0c0e4f5176f5e2323b0b74f51e4ee
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-04-15 19:47:16 +0300

    15042020

---
+++ Ediz/Paket/Modul.py
@@ -0,0 +1,16 @@
+class A:
+    def __init__(self):
+        self.var1 = "a"
+    
+    def AFonk(self):
+        return self.var1
+
+class B:
+    def __init__(self):
+        self.var1 = "a"
+    
+    def BFonk(self):
+        return self.var1
+
+def Fonk1():
+    print("Fonksiyon Çağırıldı")
