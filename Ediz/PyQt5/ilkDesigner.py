6491ba33cdd7057ebf2d7070c45dc7c55d18dd0d
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-04-17 20:17:26 +0300

    2017

--- Ediz/PyQt5/ilkDesigner.py
+++ Ediz/PyQt5/ilkDesigner.py
@@ -1,9 +1,11 @@
 import sys
 from PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5 import uic
 
 class Uygulama(QMainWindow):
     def __init__(self):
         super().__init__()
+        uic.loadUi(r"Ediz\PyQt5\ilkarayuz.ui",self)
         self.Goster()
     
     def Goster(self):
