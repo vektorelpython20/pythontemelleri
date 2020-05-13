adcb98bb2f7a9a083426f61ce2896f48beb4c056
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-05-13 23:18:51 +0300

    13052020

--- OpenCV/Ediz/Opencv11.py
+++ OpenCV/Ediz/Opencv11.py
@@ -5,12 +5,16 @@ from matplotlib import pyplot as plt
 cap = cv2.VideoCapture(1)
 yuzler_cascade = cv2.CascadeClassifier(r"OpenCV\Cascades\haarcascade_frontalface_default.xml")
 goz_cascade = cv2.CascadeClassifier(r"OpenCV\Cascades\haarcascade_eye.xml")
-gozluk = cv2.imread(r"OpenCV\Images\sunglasses.png",0)
-img = cv2.imread(r"OpenCV\Images\yuz2.PNG")
+gozluk = cv2.imread(r"OpenCV\Images\sunglasses.png")
+
 
 while True:
     # cv2.imshow("1",gozluk)
     ret,frame = cap.read()
+    frame = cv2.resize(frame,None,fx=0.9,fy=0.9,interpolation=cv2.INTER_AREA)
+    vh,vw = frame.shape[:2]
+    vh,vw = int(vh),int(vw)
+
     gri = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     merkez = []
     yuzler = yuzler_cascade.detectMultiScale(gri,1.3,5)
@@ -21,35 +25,37 @@ while True:
         roi_color = frame[y:y+h,x:x+w]
         gozler = goz_cascade.detectMultiScale(roi_gray)
         for (ex,ey,ew,eh) in gozler:
-            merkez.append((x+int(ex + 0.5*ew),y+int(ey+0.5*eh)))
+            merkez.append((x + int(ex + 0.5*ew), y + int(ey + 0.5*eh)))
 
         if len(merkez) > 0:
-          gozluk_w = 2.12 * abs(merkez[1][0]-merkez[0][0])
-          overimg = np.ones(gri.shape,np.uint8) * 255
           h,w = gozluk.shape[:2]
-          buyume_faktor = gozluk_w/w
+          goz_uzaklik = abs(merkez[1][0] - merkez[0][0])
+          gozlukw = 2.12 * goz_uzaklik
+          buyume_faktor = gozlukw/w
           overgozluk = cv2.resize(gozluk,None,fx=buyume_faktor,fy=buyume_faktor,interpolation=cv2.INTER_AREA)
           x  = merkez[0][0] if merkez[0][0] < merkez[1][0] else merkez[1][0]
-          x -= 0.26*overgozluk.shape[1]
-          y += 0.85*overgozluk.shape[0]
-
+          x -= int(0.26*overgozluk.shape[1])
+          y += int(0.26*overgozluk.shape[0])
           h,w = overgozluk.shape[:2]
+          h, w = int(h), int(w)
 
-          overimg[int(y):int(y+h),int(x):int(x+w)]=overgozluk
+          roi = frame[y:y+h, x:x+w]
 
-          grigozluk = cv2.cvtColor(overimg,cv2.COLOR_BGR2GRAY)
-
-          ret,mask = cv2.threshold(grigozluk,110,255,cv2.TRESH_BINARY)
-
+          gray_over_gozluk = cv2.cvtColor(overgozluk, cv2.COLOR_BGR2GRAY)
+          ret, mask = cv2.threshold(gray_over_gozluk, 180, 255, cv2.THRESH_BINARY_INV)
           mask_inv = cv2.bitwise_not(mask)
 
-          temp = cv2.bitwise_and(frame,frame,mask=mask)
-          temp2 = cv2.bitwise_and(overimg,overgozluk,mask=mask_inv)
-
-          son = cv2.add(temp,temp2)
-
-          cv2.imshow("deneme",son)
-
+        try:
+            # Use the mask to extract the face mask region of interest 
+            maskeli_yuz = cv2.bitwise_and(overgozluk, overgozluk, mask=mask) 
+            # Use the inverse mask to get the remaining part of the image 
+            maskeli_frame = cv2.bitwise_and(roi, roi, mask=mask_inv) 
+        except cv2.error as e:
+            print('aritmetik hata: '+ str(e))
+            #raise e  
+        frame[y:y+h, x:x+w] = cv2.add(maskeli_yuz, maskeli_frame)  
+    else:
+        print('Göz tespit edilemedi')    
 
 
 
